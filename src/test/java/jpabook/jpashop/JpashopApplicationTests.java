51ccffa4d8f0741debe488e303e93de671135e6c
ooeunz <yuns994@gmail.com>
2020-07-30 22:54:06 +0900

    Fix error about no found class

--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,13 +1,13 @@
-package jpabook.jpashop;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JpashopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package jpabook.jpashop;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JpashopApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
