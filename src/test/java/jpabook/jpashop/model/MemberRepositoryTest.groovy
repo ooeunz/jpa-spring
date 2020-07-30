87173bc36aec74e72531d3bef272cb8119c729a3
ooeunz <yuns994@gmail.com>
2020-07-30 17:44:48 +0900

    Occur error to test code by spock of groovy

--- src/test/java/jpabook/jpashop/model/MemberRepositoryTest.groovy
+++ src/test/java/jpabook/jpashop/model/MemberRepositoryTest.groovy
@@ -1,6 +1,39 @@
 package jpabook.jpashop.model
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 import spock.lang.Specification
 
+@SpringBootTest
 class MemberRepositoryTest extends Specification {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Transactional
+    def "Repository가 작동하는지 확인한다"() {
+        given:
+        def member = new Member()
+
+        when:
+        member.setUsername("memberA")
+        def saveId = memberRepository.save(member)
+        def findMember = memberRepository.find(saveId)
+
+        then:
+        findMember.getId() == member.getId()
+    }
+
+    def "왜 안돼?"() {
+        given:
+        def a = 1
+        def b = 2
+
+        when:
+        def c = a + b
+
+        then:
+        c = 3
+    }
 }
